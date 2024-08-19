c9651d5dea726877947aae2a150e4b794ffb4b7f
narasimha0503 <narasimha@rankz.io>
2024-08-19 17:04:20 +0530

    added a logic for the communication

---
+++ popup/popup.js
@@ -0,0 +1,37 @@
+var sendBtn = document.getElementById("send-button");
+
+sendBtn.addEventListener("click", clickFunc);
+
+function clickFunc() {
+  var userInput = document.getElementById("user-input");
+
+  var userMessage = document.createElement("p");
+  userMessage.classList.add("user-message");
+  userMessage.textContent = userInput.value;
+  chatbox.appendChild(userMessage);
+
+  var version = document.getElementById("default").checked;
+  var botMessage = defaultChat();
+  userInput.value = "";
+
+  chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+function defaultChat() {
+  var userInput = document.getElementById("user-input");
+  var chatbox = document.getElementById("chatbox");
+  var botMessage = document.createElement("p");
+
+  const prompt = userInput.value;
+  console.log(prompt);
+  chrome.runtime.sendMessage(
+    { message: "textToTextPrompt", prompt: prompt },
+    function (response) {
+      botMessage.classList.add("bot-message");
+      var pre = document.createElement("pre");
+      pre.textContent = response;
+      botMessage.appendChild(pre);
+      chatbox.appendChild(botMessage);
+    }
+  );
+}
